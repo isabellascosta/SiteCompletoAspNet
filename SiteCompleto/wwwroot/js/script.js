44465186d725e6add7052793d0d3f4c563b741aa
Isabella Costa <isa_bella411@hotmail.com>
2023-10-26 19:32:33 -0300

    Criado slide rotativo

---
+++ SiteCompleto/wwwroot/js/script.js
@@ -0,0 +1,13 @@
+	var_containerMenu = $('[data-container="menu"]');
+
+	$(window).scroll (funcion(){
+		if{$(this).scrollTop()>70{
+			_containerMenu.addclass('bloco-fixo'{
+		}else{
+			_container.fadeOut(0);
+		}
+
+	_container$.click(functions(){
+	$("hmtl.body").animate({scrollTop: 0}, 800);
+	return false;
+	});
