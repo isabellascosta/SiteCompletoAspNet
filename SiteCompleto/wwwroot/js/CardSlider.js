a23e3238c078633b9533d783cbd79e066ff739ba
Isabella Costa <isa_bella411@hotmail.com>
2023-12-05 16:10:46 -0300

    iniciado o card slide

---
+++ SiteCompleto/wwwroot/js/CardSlider.js
@@ -0,0 +1,33 @@
+﻿var swiper = new Swiper(".slide-containerc", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    sliderPerGroup: 4,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+        el: ".swiper-paginationc",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-nextc",
+        prevEl: ".swiper-button-prevc",
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1000: {
+            slidesPerView: 4,
+        },
+    },
+});
